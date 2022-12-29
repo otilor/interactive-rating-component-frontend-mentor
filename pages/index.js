7b73fa4c6c8e6c95e56afc207f3d1d9e65d2a0b3
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-12-29 21:02:58 +0100

    remove css

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,8 +13,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        
+      <main>
+
       </main>
     </>
   )
