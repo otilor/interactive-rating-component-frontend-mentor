873fcbd45241cb3b704114edb79fc0541e22a26d
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-12-30 16:56:07 +0100

    change stuff

--- pages/index.js
+++ pages/index.js
@@ -29,12 +29,12 @@ export default function Home() {
                 feedback is appreciated to help us improve our offering!
               </p>
             </div>
-            <div className="space-x-4 ml-4 mt-6 text-slate-400">
-              <div className="bg-gray-500 w-12 h-12 rounded-full inline-flex items-center justify-center">1</div>
-              <div className="bg-gray-500 w-12 h-12 rounded-full inline-flex items-center justify-center">2</div>
-              <div className="bg-gray-500 w-12 h-12 rounded-full inline-flex items-center justify-center">3</div>
-              <div className="bg-gray-500 w-12 h-12 rounded-full inline-flex items-center justify-center">4</div>
-              <div className="bg-gray-500 w-12 h-12 rounded-full inline-flex items-center justify-center">5</div>
+            <div className="space-x-4 ml-4 mt-6 text-gray-200">
+              <div className="bg-slate-500 w-12 h-12 rounded-full inline-flex items-center justify-center">1</div>
+              <div className="bg-slate-500 w-12 h-12 rounded-full inline-flex items-center justify-center">2</div>
+              <div className="bg-slate-500 w-12 h-12 rounded-full inline-flex items-center justify-center">3</div>
+              <div className="bg-slate-500 w-12 h-12 rounded-full inline-flex items-center justify-center">4</div>
+              <div className="bg-slate-500 w-12 h-12 rounded-full inline-flex items-center justify-center">5</div>
             </div>
 
   
