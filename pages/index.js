949cd04c6467e22f94ed362709a34c6662b65df7
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-12-30 17:00:19 +0100

    add inline flex

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
                 feedback is appreciated to help us improve our offering!
               </p>
             </div>
-            <div className="space-x-4 ml-4 mt-6 text-gray-400">
+            <div className="space-x-4 inline-flex ml-4 mt-6 text-gray-400">
               <div className="bg-slate-600 w-12 h-12 rounded-full inline-flex items-center justify-center">1</div>
               <div className="bg-slate-600 w-12 h-12 rounded-full inline-flex items-center justify-center">2</div>
               <div className="bg-slate-600 w-12 h-12 rounded-full inline-flex items-center justify-center">3</div>
